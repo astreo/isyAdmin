6abaf40813ad5094240bb89de69ad945bb2878aa
Rodrigo Astrada <rastrada@isy.mobi>
2019-10-28 16:32:20 -0300

    lista usuarios creada

--- src/app/components/main/main.module.ts
+++ src/app/components/main/main.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from '../dashboard/dashboard.component';
 // import { StoreModule } from '@ngrx/store';
-// import { reducer } from './ingreso-egreso.reducer';
+import { reducer } from '../../store/reducers/usuario.reducer';
 import { HomeComponent } from '../home/home.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../../shared/shared.module';
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { StoreModule } from '@ngrx/store';
+import { UsersListComponent } from '../users/users-list/users-list.component';
 
 
 
@@ -17,14 +19,15 @@ import { TranslateModule } from '@ngx-translate/core';
     DashboardComponent,
     HomeComponent,
     MainComponent,
+    UsersListComponent,
   ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     SharedModule,
     MainRoutingModule,
-    TranslateModule.forChild()
-    // StoreModule.forFeature('ingresoEgreso', reducer)
+    TranslateModule.forChild(),
+    // StoreModule.forFeature('account', reducer)
   ]
 })
 export class MainModule { }
--- src/app/components/main/main.routes.ts
+++ src/app/components/main/main.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
 import { DashboardComponent } from '../dashboard/dashboard.component';
+import { UsersListComponent } from '../users/users-list/users-list.component';
 
 
 export const mainRoutes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'usuario', component: UsersListComponent },
   { path: 'dashboard', component: DashboardComponent },
 ];
