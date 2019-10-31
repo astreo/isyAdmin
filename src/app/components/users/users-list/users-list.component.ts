4d278ec47af3805e120e1fa0b1b59c0ae818d361
Rodrigo Astrada <rastrada@isy.mobi>
2019-10-31 10:35:04 -0300

    listado de usuarios filtro terminado

--- src/app/components/users/users-list/users-list.component.ts
+++ src/app/components/users/users-list/users-list.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit, OnDestroy, PipeTransform } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../../store/reducers/usuario.reducer';
 import { usuarios as actions } from '../../../store/actions';
-import { map } from 'rxjs/operators';
-import { Subscription } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { Subscription, Observable } from 'rxjs';
 import { DecimalPipe } from '@angular/common';
 import { FormControl } from '@angular/forms';
 
@@ -22,18 +22,20 @@ declare interface UsuariosLista {
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
-  styleUrls: ['./users-list.component.scss']
+  styleUrls: ['./users-list.component.scss'],
+  providers: [DecimalPipe]
 })
 export class UsersListComponent implements OnInit, OnDestroy {
   loading: boolean;
   usuarios = {} as UsuariosLista[];
+  usuarios$: Observable<UsuariosLista[]>;
   pageSize = 15;
   page = 1;
   accountSuscription = new Subscription();
   usersSuscription = new Subscription();
   filter = new FormControl('');
 
-  constructor(public store: Store<AppState>, pipe: DecimalPipe) { }
+  constructor(public store: Store<AppState>, public pipe: DecimalPipe) { }
 
   ngOnInit() {
     this.accountSuscription = this.store.select('account')
@@ -64,6 +66,10 @@ export class UsersListComponent implements OnInit, OnDestroy {
     .subscribe(mappedItems => {
       this.usuarios = mappedItems.users;
       this.loading = mappedItems.loading;
+      this.usuarios$ = this.filter.valueChanges.pipe(
+        startWith(''),
+        map(text => this.search(text, this.pipe))
+      );
       console.log('subscribe');
     });
     this.store.dispatch(new actions.CargarUsuarios(idProveedor));
@@ -73,7 +79,12 @@ export class UsersListComponent implements OnInit, OnDestroy {
     return this.usuarios.filter(usuario => {
       const term = text.toLowerCase();
       return usuario.nombres.toLowerCase().includes(term)
-          || usuario.apellidos.toLowerCase().includes(term);
+          || usuario.apellidos.toLowerCase().includes(term)
+          || usuario.username.toLowerCase().includes(term)
+          || usuario.proveedor.toLowerCase().includes(term)
+          || usuario.perfil.toLowerCase().includes(term)
+          || usuario.fechaCreacion.includes(term)
+          ;
     });
   }
 }
